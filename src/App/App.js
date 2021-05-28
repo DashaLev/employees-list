610244e3d8f1abb8b56aa4866fce179252d84d73
Dasha <d.l92@ukr.net>
2021-05-28 23:19:43 +0300

    added local store

--- src/App/App.js
+++ src/App/App.js
@@ -1,61 +1,58 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import EmployeesList from './Employees/EmployeesList'
-import EmployeesBirthday from './EmployeesBirthday/EmployeesBirthday'
-import '../style/base.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import EmployeesList from "./Employees/EmployeesList";
+import EmployeesBirthday from "./EmployeesBirthday/EmployeesBirthday";
+import "../style/base.css";
 
-class App extends Component {
-  state = {
-    employees: [],
-  }
+const App = () => {
+    const [employees, setEmployees] = useState([]);
 
+    useEffect(() => {
+        const data = localStorage.getItem("myEmployeesList");
+        if (data) {
+            setEmployees(JSON.parse(data));
+        } else {
+            const url = `https://yalantis-react-school-api.yalantis.com/api/task0/users`;
+            axios.get(url).then((res) => {
+                const users = res.data.map((user) => ({
+                    ...user,
+                    active: false,
+                }));
+                setEmployees(users);
+            });
+        }
+    }, [setEmployees]);
 
-  componentDidMount() {
-    axios
-      .get(`https://yalantis-react-school-api.yalantis.com/api/task0/users`)
-      .then((res) => res.data)
-      .then((data) =>
-        this.setState(() => ({
-            employees: data.map(user => ({
-              ...user, active: false
-            })
-          )}
-        ))
-      )
-  }
+    useEffect(() => {
+        localStorage.setItem("myEmployeesList", JSON.stringify(employees));
+    });
 
+    function addChecked(id) {
+        setEmployees(
+            employees.map((user) =>
+                user.id === id ? { ...user, active: true } : { ...user },
+            ),
+        );
+    }
 
-  addChecked = (id) => {
-    this.setState((prevState) => ({
-      ...prevState,
-      employees:this.state.employees.map(user => (user.id === id ? { ...user, active: true} : {...user}))
-      
-    }))
+    function removeChecked(id) {
+        setEmployees(
+            employees.map((user) =>
+                user.id === id ? { ...user, active: false } : { ...user },
+            ),
+        );
+    }
 
-  }
-
-
-
-  removeChecked = (id) => {
-    this.setState((prevState) => ({
-      ...prevState,
-      employees:this.state.employees.map(user => (user.id === id ? { ...user, active: false} : {...user}))
-      
-    }))
-  }
-
-  render() {
     return (
-      <div className="page-columns">
-        <EmployeesList 
-          employees={this.state.employees}
-          addChecked={this.addChecked}
-          removeChecked={this.removeChecked}/>
-        <EmployeesBirthday employees={this.state.employees} /> 
-      </div>
-    )
-  }
-}
+        <div className="page-columns">
+            <EmployeesList
+                employees={employees}
+                addChecked={addChecked}
+                removeChecked={removeChecked}
+            />
+            <EmployeesBirthday employees={employees} />
+        </div>
+    );
+};
 
-export default App
-
+export default App;
--- src/App/EmployeesBirthday/EmployeesBirthday.js
+++ src/App/EmployeesBirthday/EmployeesBirthday.js
@@ -16,88 +16,98 @@ const EmployeesBirthday = ({ employees }) => {
         { id: "12", month: "December" },
     ];
 
-    const employeesActiveState = employees.map(el=> el.active)
-
+    const employeesActiveState = employees.map((el) => el.active);
 
     return (
         <>
             <div className="employees-birthday-page">
                 <h1 className="page-title">Employees birthday</h1>
                 <div className="employees-birthday-list">
-                    { employeesActiveState.every(element => element === false) ? 
+                    {employeesActiveState.every(
+                        (element) => element === false,
+                    ) ? (
+                        <div className="employees-birthday-list-empty">
+                            Employees list is empty
+                        </div>
+                    ) : (
+                        months
+                            .sort(function (month1, month2) {
+                                let a = month1.id,
+                                    b = month2.id;
+                                let currentMonth = new Date().getMonth() + 1;
+                                if (a < currentMonth) {
+                                    a = a + 12;
+                                }
+                                if (b < currentMonth) {
+                                    b = b + 12;
+                                }
+                                return a - b;
+                            })
+                            .map((item, index) => (
+                                <div
+                                    key={index}
+                                    className="employees-birthday-section"
+                                >
+                                    <h2>{item.month}</h2>
 
-                    <div  className="employees-birthday-list-empty">Employees list is empty</div> :
-
-                    months
-                        .sort(function (month1, month2) {
-                            let a = month1.id,
-                                b = month2.id;
-                            let currentMonth = new Date().getMonth() + 1;
-                            if (a < currentMonth) {
-                                a = a + 12;
-                            }
-                            if (b < currentMonth) {
-                                b = b + 12;
-                            }
-                            return a - b;
-                        })
-                        .map((item, index) => (
-
-                            
-                            <div
-                                key={index}
-                                className="employees-birthday-section"
-                            >
-                                <h2>{item.month}</h2>
-
-                                {employees
-                                    .filter(
-                                        (employer) =>
-                                            employer.dob.slice(5, 7) ===
-                                                item.id &&
-                                            employer.active === true,
-                                    )
-                                    .sort((a, b) =>
-                                        a.lastName.localeCompare(b.lastName),
-                                    )
-                                    .map(({ id, lastName, firstName, dob }) => (
-                                        <ul
-                                            key={id}
-                                            className="employees-birthday-item"
-                                        >
-                                            <li className="employer-lastName">
-                                                {lastName}
-                                            </li>
-                                            <li className="employer-firstName">
-                                                {firstName}
-                                            </li>
-                                            <li className="employer-birthday">
-                                                {new Intl.DateTimeFormat(
-                                                    "en-GB",
-                                                    {
-                                                        day: "numeric",
-                                                        month: "long",
-                                                    },
-                                                ).format(new Date(dob))}
-                                                ,{" "}
-                                                {new Intl.DateTimeFormat(
-                                                    "en-GB",
-                                                    {
-                                                        year: "numeric",
-                                                    },
-                                                ).format(new Date(dob))}{" "}
-                                                year
-                                            </li>
-                                        </ul>
-                                    ))}
-                            </div>
-                        ))
-                        
-                    }
+                                    {employees
+                                        .filter(
+                                            (employer) =>
+                                                employer.dob.slice(5, 7) ===
+                                                    item.id &&
+                                                employer.active === true,
+                                        )
+                                        .sort((a, b) =>
+                                            a.lastName.localeCompare(
+                                                b.lastName,
+                                            ),
+                                        )
+                                        .map(
+                                            ({
+                                                id,
+                                                lastName,
+                                                firstName,
+                                                dob,
+                                            }) => (
+                                                <ul
+                                                    key={id}
+                                                    className="employees-birthday-item"
+                                                >
+                                                    <li className="employer-lastName">
+                                                        {lastName}
+                                                    </li>
+                                                    <li className="employer-firstName">
+                                                        {firstName}
+                                                    </li>
+                                                    <li className="employer-birthday">
+                                                        {new Intl.DateTimeFormat(
+                                                            "en-GB",
+                                                            {
+                                                                day: "numeric",
+                                                                month: "long",
+                                                            },
+                                                        ).format(new Date(dob))}
+                                                        ,{" "}
+                                                        {new Intl.DateTimeFormat(
+                                                            "en-GB",
+                                                            {
+                                                                year: "numeric",
+                                                            },
+                                                        ).format(
+                                                            new Date(dob),
+                                                        )}{" "}
+                                                        year
+                                                    </li>
+                                                </ul>
+                                            ),
+                                        )}
+                                </div>
+                            ))
+                    )}
                 </div>
             </div>
         </>
     );
-}
+};
 
 export default EmployeesBirthday;
